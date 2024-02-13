f0cc8353d8599c6d825c14a8d1316f8ae4a0de77
Arthur-byte-code <152222113+Arthur-byte-code@users.noreply.github.com>
2024-02-13 16:04:53 -0300

    Add files via upload

---
+++ automationPython/PythonProject.py
@@ -0,0 +1,96 @@
+import pyautogui
+import time
+pyautogui.PAUSE=1
+pyautogui.press("win")
+pyautogui.write("microsoft edge")
+pyautogui.press("enter")
+time.sleep(5)
+link = "file:///C:/Users/Raul%20Diego/Desktop/automationPython/Formul%C3%A1rio.html"
+pyautogui.write(link)
+time.sleep(8)
+pyautogui.press("enter")
+
+time.sleep(5)
+ 
+import pandas 
+tabela = pandas.read_csv("produtos.csv")
+for linha in tabela.index: 
+
+    
+    pyautogui.click(x=634, y=130)
+    
+    pyautogui.write(tabela.loc[linha, "codigo"])
+    pyautogui.press("tab")
+
+    
+    pyautogui.write(tabela.loc[linha, "marca"])
+    pyautogui.press("tab")
+
+   
+    pyautogui.write(tabela.loc[linha, "tipo"])
+    pyautogui.press("tab")
+
+    
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+
+    pyautogui.write((str(tabela.loc[linha, "preco_unitario"])))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+
+    obs=tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        pyautogui.write(obs)
+
+    
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ automationPython/auxiliar.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+time.sleep(8)
+posicao_cursor = pyautogui.position()
+print(f"A posição atual do cursor é: {posicao_cursor}")
+
